fad4648edceaa5fca924e7e6b51fb0143266826b
Danyel Murray and Ross Fletcher <danyel@>
2018-02-15 08:58:32 -0800

    update append to have better format on outputand clear ul on submit

--- assets/js/bikeAPI-interface.js
+++ assets/js/bikeAPI-interface.js
@@ -7,14 +7,22 @@ $(document).ready(function() {
     let location = $('#location').val();
     let perPage = $('#per-page').val();
     let newSearch = promise(perPage, location, distance);
-
+    $('#result').empty();
 
     newSearch.then(function(response) {
       let body = JSON.parse(response);
-      $('#result').append("<li>" + body.bikes[0].title + "</li>");
-      $('#result').append("<li>" + body.bikes[0].id + "</li>");
+      for (let i = 0; i < body.bikes.length; i++) {
+      $('#result').append("<div class='box'>" + "<h2>" + body.bikes[i].title + "</h2>" + "<li>" + "ID #" + body.bikes[i].id + "</li>" + "</div>");
+      if (body.bikes[i].thumb === null) {
+         $('#result').append("<p><em> No image found </em></p>");
+       } else {
+           $('#result').append('<img src =' + body.bikes[i].thumb + '>');
+       }
+    }
     }, function (error) {
       $('.showErrors').text('There was an error: ${error.message}');
-    });
+
+
   });
+});
 });
